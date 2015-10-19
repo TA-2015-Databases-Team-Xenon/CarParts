a8ac60c8b9a6ef59c38bcafb9ac6996768d36301
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-10-19 13:21:05 +0300

    Added json report generation to filesystem

--- CarParts.Data/Json/JsonHandler.cs
+++ CarParts.Data/Json/JsonHandler.cs
@@ -1,7 +1,34 @@
 ﻿namespace CarParts.Data.Json
 {
+    using System.IO;
+    using CarParts.Models.ReportModels;
+    using Newtonsoft.Json;
+
     public class JsonHandler
     {
-        // TODO: some method to generate JSON part reports and save them to the file system;
+        private const string DefaultOutputDirectory = "..\\..\\..\\DataOutput\\Json-Reports";
+        private const string OutputFileFormat = "{0}\\{1}.json";
+        
+        public void GenerateJsonReport(PartReportInputModel productReport, string outputDirectory = DefaultOutputDirectory)
+        {
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+
+            using (var writer = File.CreateText(string.Format(OutputFileFormat,outputDirectory, productReport.PartId)))
+            {
+                writer.Write(JsonConvert.SerializeObject(new
+                {
+                    id = productReport.PartId,
+                    name = productReport.PartName,
+                    vendor = productReport.Vendor,
+                    price = productReport.Price,
+                    quantity = productReport.Quantity,
+                    total = productReport.TotalPrice
+                },
+                Formatting.Indented));
+            }
+        }
     }
 }
---
+++ CarParts.Models/ReportModels/PartReportInputModel.cs
@@ -0,0 +1,27 @@
+﻿namespace CarParts.Models.ReportModels
+{
+    using System;
+
+    public class PartReportInputModel
+    {
+        public int PartId { get; set; }
+
+        public string PartName { get; set; }
+
+        public string Vendor { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = this.Quantity * this.Price;
+
+                return Math.Round(totalPrice, 2);
+            }
+        }
+    }
+}
