808943771ef39cdd266cce08b90a27b5f74a265c
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2015-10-20 04:23:23 +0300

    Problem 3 - xml sales report writing completed. All problems are solved.

--- CarParts.ConsoleClient/Startup.cs
+++ CarParts.ConsoleClient/Startup.cs
@@ -29,69 +29,42 @@
             var zipExtractor = new ZipExtractor();
             var jsonHandler = new JsonHandler();
             var sqliteHandler = new SqliteHandler();
+            var xmlHandler = new XmlHandler();
 
             // Mongolab.com credentials - Username: TeamXenon , Passsword: xenon123
-            //// These tasks are done. You can physically delete the data from your SqlServerDatabase and the extracted excell files and run this method again.
-            //ProblemOne(mongoHandler, mongoToSql, zipExtractor, excellHandler);
 
-            //ProblemTwo(pdfHandler);
+            ProblemOne(mongoHandler, mongoToSql, zipExtractor, excellHandler);
+
+            ProblemTwo(pdfHandler);
+
+            ProblemThree(xmlHandler);
 
             //// NOTE!!! - you need to go to CarParts.Data.MySql project and in its App.config file 
-            //// you should change the password with which you connect to your localhost instance of the MySQL Workbench server.
-            //ProblemFour(sqlHandler, mySqlHandler, jsonHandler);
+            //// you should change the password in the connectionString tag with which you connect to your localhost instance of the MySQL Workbench server.
+            ProblemFour(sqlHandler, mySqlHandler, jsonHandler);
 
-            //// This will throw because coutries are already added to the cloud and have unique constraint to their name in sql server.
-            //// You can delete physically the data and run the method if you like.
-            //ProblemFive(mongoHandler, xmlToSql);
-            
-            //ProblemSix(excellHandler, sqlHandler, sqliteHandler, mySqlHandler);                       
+            ProblemFive(mongoHandler, xmlToSql);
+
+            ProblemSix(excellHandler, sqlHandler, sqliteHandler, mySqlHandler);
         }
 
-        private static void ProblemSix(ExcellHandler excellHandler, SqlServerHandler sqlHandler, SqliteHandler sqliteHandler, MySqlHandler mySqlHandler)
+        private static void ProblemThree(XmlHandler xmlHandler)
         {
-            var partNames = sqlHandler.ReadPartNames();
-            sqliteHandler.Seed(partNames);
-
-            var reports = mySqlHandler.ReadReports();
-            var taxes = sqliteHandler.ReadTaxes();
-
-            excellHandler.GenerateExcellFile(reports, taxes, "VendorsFinancialReport.xlsx"); 
-        }
-
-        private static void ProblemFour(SqlServerHandler sqlHandler, MySqlHandler mySqlHandler, JsonHandler jsonHandler)
-        {
-            var reports = sqlHandler.ReadPartReports();
-
-            jsonHandler.GenerateJsonReports(reports);
-            mySqlHandler.WriteReports(reports);
-        }
-
-        private static void ProblemTwo(PdfHandler pdfHandler)
-        {
-            pdfHandler.GenerateSalesInfoPdf();
-        }
-
-        private static void ProblemFive(MongoDbHandler mongoHandler, XmlToSqlServerLoader xmlToSql)
-        {
-            var xmlHandler = new XmlHandler();
-            var countries = xmlHandler.GetCountries();
-
-            xmlToSql.LoadCountries(countries);
-            mongoHandler.WriteCollection<XmlCountry>("Countries", countries);
-            Console.WriteLine("successfully write down countries to MongoDB and SqlServer databases");
+            xmlHandler.GenerateSalesReport();
+            Console.WriteLine("Successfully generated xml sales report file.");
         }
 
         private static void ProblemOne(MongoDbHandler mongoHandler, MongoToSqlServerLoader mongoToSql, ZipExtractor extractor, ExcellHandler excellHandler)
         {
-            // Populating database in MongoLab with some useless testing data
-            try
-            {                
-              new MongoDataSeeder().Seed(mongoHandler);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Just in case you uncomment and try to populate the cloud database with existing id's and it throws :)");
-            }
+            //// Populating database in MongoLab with some useless testing data
+            //try
+            //{                
+            //  new MongoDataSeeder().Seed(mongoHandler);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine("Just in case you uncomment and try to populate the cloud database with existing id's and it throws :)");
+            //}
 
             mongoToSql.Load(mongoHandler);
             Console.WriteLine("successsfully added all in a sql database");
@@ -101,6 +74,48 @@
 
             excellHandler.MigrateFromExcellToSqlServer();
             Console.WriteLine("successfully added sales table from excell files.");
+        }
+
+        private static void ProblemTwo(PdfHandler pdfHandler)
+        {
+            pdfHandler.GenerateSalesInfoPdf();
+            Console.WriteLine("Successfully generated pdf file.");
+        }
+
+        private static void ProblemFour(SqlServerHandler sqlHandler, MySqlHandler mySqlHandler, JsonHandler jsonHandler)
+        {
+            var reports = sqlHandler.ReadPartReports();
+
+            jsonHandler.GenerateJsonReports(reports);
+            mySqlHandler.WriteReports(reports);
+            Console.WriteLine("Successfully added json reports to the file system and to MySQL database.");
+        }
+
+        private static void ProblemFive(MongoDbHandler mongoHandler, XmlToSqlServerLoader xmlToSql)
+        {
+            var xmlHandler = new XmlHandler();
+            var countries = xmlHandler.GetCountries();
+
+            // If these are runned before they will throw because coutries are already added to the cloud and have unique constraint to their name in sql server.
+            // You can delete physically the data and run the method if you like.
+            xmlToSql.LoadCountries(countries);
+            //try // Just in case :)
+            //{
+            //    mongoHandler.WriteCollection<XmlCountry>("Countries", countries);
+            //}
+            //catch (Exception ex)
+            //{
+            //}
+            Console.WriteLine("Successfully write down countries from XML to MongoDB and SqlServer databases");
+        }
+
+        private static void ProblemSix(ExcellHandler excellHandler, SqlServerHandler sqlHandler, SqliteHandler sqliteHandler, MySqlHandler mySqlHandler)
+        {
+            var reports = mySqlHandler.ReadReports();
+            var taxes = sqliteHandler.ReadTaxes();
+
+            excellHandler.GenerateExcellFile(reports, taxes, "VendorsFinancialReport.xlsx");
+            Console.WriteLine("Successfully generated excell file to ");
         }
     }
 }
